981625c502ac913e49ea66373c9d6101060f17f8
tebogokgopa <tebogokgopa84@gmail.com>
2020-02-13 11:38:22 +0200

    Delete macChanger.py

--- macChanger.py
+++
@@ -1,6 +0,0 @@
-import subprocess as sp
-
-sp.call("sudo","ipconfig","OAM-016721","down")
-sp.call("sudo","ipconfig","OAM-016721","hw","name","00:11:22:33:44:55")
-sp.call("sudo","ipconfig","OAM-016721","up")
-
